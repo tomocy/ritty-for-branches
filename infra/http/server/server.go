c64d1f6a1210faeeb5f2b02796488b3282dfcdc6
tomocy <tomocy.dev@gmail.com>
2019-05-10 15:48:46 +0900

    let Server register routes before serving

--- infra/http/server/server.go
+++ infra/http/server/server.go
@@ -5,17 +5,29 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/tomocy/ritty-for-branches/infra/http/registerer"
 )
 
-func New() *Server {
-	return new(Server)
+func New(rs ...registerer.Registerer) *Server {
+	return &Server{
+		router:      chi.NewRouter(),
+		registerers: rs,
+	}
 }
 
 type Server struct {
-	router chi.Router
+	router      chi.Router
+	registerers []registerer.Registerer
 }
 
 func (s *Server) ListenAndServe(addr string) error {
+	s.registerRoutes()
 	log.Printf("listen and serve on %s\n", addr)
 	return http.ListenAndServe(addr, s.router)
+}
+
+func (s *Server) registerRoutes() {
+	for _, r := range s.registerers {
+		r.RegisterRoutes(s.router)
+	}
 }
