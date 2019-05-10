66926e60eafdea3bcd5bc2a184c2ff4b0b46b90a
tomocy <tomocy.dev@gmail.com>
2019-05-10 18:33:48 +0900

    let WebRegisterer require web/Handler

--- infra/http/registerer/web_registerer.go
+++ infra/http/registerer/web_registerer.go
@@ -1,11 +1,18 @@
 package registerer
 
-import "github.com/go-chi/chi"
+import (
+	"github.com/go-chi/chi"
+	"github.com/tomocy/ritty-for-branches/infra/http/web/handler"
+)
 
-func NewWebRegisterer() *WebRegisterer {
-	return new(WebRegisterer)
+func NewWebRegisterer(handler *handler.Handler) *WebRegisterer {
+	return &WebRegisterer{
+		handler: handler,
+	}
 }
 
-type WebRegisterer struct{}
+type WebRegisterer struct {
+	handler *handler.Handler
+}
 
 func (r *WebRegisterer) RegisterRoutes(router chi.Router) {}
