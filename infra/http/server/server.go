f677d059e7668ee407e32fdf3eb7d02dcba2ae79
tomocy <tomocy.dev@gmail.com>
2019-05-10 15:44:11 +0900

    let Server listen and serve on given port

--- infra/http/server/server.go
+++ infra/http/server/server.go
@@ -1,7 +1,21 @@
 package server
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+)
+
 func New() *Server {
 	return new(Server)
 }
 
-type Server struct{}
+type Server struct {
+	router chi.Router
+}
+
+func (s *Server) ListenAndServe(addr string) error {
+	log.Printf("listen and serve on %s\n", addr)
+	return http.ListenAndServe(addr, s.router)
+}
