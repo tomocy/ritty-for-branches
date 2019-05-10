c0ea9254e466935868514cc4aac28458fb15f443
tomocy <tomocy.dev@gmail.com>
2019-05-10 15:51:37 +0900

    add methods to load config

--- config/config.go
+++ config/config.go
@@ -1,4 +1,26 @@
 package config
 
+import "github.com/spf13/viper"
+
+var Current *Config
+
 type Config struct {
+}
+
+func Must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Load(fname string) error {
+	viper.SetConfigFile(fname)
+	if err := viper.ReadInConfig(); err != nil {
+		return err
+	}
+	if err := viper.Unmarshal(&Current); err != nil {
+		return err
+	}
+
+	return nil
 }
