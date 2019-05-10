4d64afea841ed6a872b8728108bdd8e811433f8a
tomocy <tomocy.dev@gmail.com>
2019-05-10 18:53:28 +0900

    let branchHandler show menu registration form

--- infra/http/web/handler/branch_handler.go
+++ infra/http/web/handler/branch_handler.go
@@ -1,7 +1,23 @@
 package handler
 
-func newBranchHandler() *branchHandler {
-	return new(branchHandler)
+import (
+	"net/http"
+
+	"github.com/tomocy/ritty-for-branches/infra/http/view"
+)
+
+func newBranchHandler(view view.View) *branchHandler {
+	return &branchHandler{
+		view: view,
+	}
 }
 
-type branchHandler struct{}
+type branchHandler struct {
+	view view.View
+}
+
+func (h *branchHandler) ShowMenuRegistrationForm(w http.ResponseWriter, r *http.Request) {
+	if err := h.view.Show(w, "menu.new", nil); err != nil {
+		logInternalServerError(w, "show branch menu registration form", err)
+	}
+}
--- infra/http/web/handler/handler.go
+++ infra/http/web/handler/handler.go
@@ -1,7 +1,23 @@
 package handler
 
-func New() *Handler {
-	return new(Handler)
+import (
+	"log"
+	"net/http"
+
+	"github.com/tomocy/ritty-for-branches/infra/http/view"
+)
+
+func New(view view.View) *Handler {
+	return &Handler{
+		branchHandler: newBranchHandler(view),
+	}
 }
 
-type Handler struct{}
+type Handler struct {
+	*branchHandler
+}
+
+func logInternalServerError(w http.ResponseWriter, did string, msg interface{}) {
+	log.Printf("failed to %s: %v\n", did, msg)
+	w.WriteHeader(http.StatusInternalServerError)
+}
