10652d6451f8a4c417b03c7167833a237cd7ad2a
tomocy <tomocy.dev@gmail.com>
2019-05-11 03:13:30 +0900

    enable to register api routes

--- infra/http/registerer/api_registerer.go
+++ infra/http/registerer/api_registerer.go
@@ -1,7 +1,18 @@
 package registerer
 
-func NewAPIRegisterer() *APIRegisterer {
-	return new(APIRegisterer)
+import (
+	"github.com/go-chi/chi"
+	"github.com/tomocy/ritty-for-branches/infra/http/api/handler"
+)
+
+func NewAPIRegisterer(handler *handler.Handler) *APIRegisterer {
+	return &APIRegisterer{
+		handler: handler,
+	}
 }
 
-type APIRegisterer struct{}
+type APIRegisterer struct {
+	handler *handler.Handler
+}
+
+func (r *APIReigsterer) RegisterRoutes(router chi.Router) {}
