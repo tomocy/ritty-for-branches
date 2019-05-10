54e0d9dd6c1e14e6fcb884a841bfd2618f899f82
tomocy <tomocy.dev@gmail.com>
2019-05-10 18:43:20 +0900

    insert WebHandler into WebRegisterer in starting server

--- main.go
+++ main.go
@@ -6,13 +6,15 @@ import (
 	"github.com/tomocy/ritty-for-branches/config"
 	"github.com/tomocy/ritty-for-branches/infra/http/registerer"
 	"github.com/tomocy/ritty-for-branches/infra/http/server"
+	"github.com/tomocy/ritty-for-branches/registry"
 )
 
 func main() {
 	config.Must(config.Load("./config.yml"))
 
 	addr := ":" + config.Current.Self.Port
-	webRegi := registerer.NewWebRegisterer()
+	registry := registry.NewHTTPRegistry()
+	webRegi := registerer.NewWebRegisterer(registry.NewWebHandler())
 	server := server.New(webRegi)
 	if err := server.ListenAndServe(addr); err != nil {
 		log.Printf("failed to listen and serve: %s\n", err)
--- registry/http_registry.go
+++ registry/http_registry.go
@@ -1,7 +1,34 @@
 package registry
 
+import (
+	"github.com/tomocy/ritty-for-branches/infra/http/view"
+	"github.com/tomocy/ritty-for-branches/infra/http/web/handler"
+)
+
 func NewHTTPRegistry() *HTTPRegistry {
-	return new(HTTPRegistry)
+	return &HTTPRegistry{
+		web: newHTTPWebRegistry(),
+	}
 }
 
-type HTTPRegistry struct{}
+type HTTPRegistry struct {
+	web *httpWebRegistry
+}
+
+func (r *HTTPRegistry) NewWebHandler() *handler.Handler {
+	return r.web.newHandler()
+}
+
+func newHTTPWebRegistry() *httpWebRegistry {
+	return &httpWebRegistry{
+		view: view.NewHTML(),
+	}
+}
+
+type httpWebRegistry struct {
+	view *view.HTML
+}
+
+func (r *httpWebRegistry) newHandler() *handler.Handler {
+	return handler.New(r.view)
+}
