59c2bcb4b570d3582a73aecd4146fca8ee2f970e
tomocy <tomocy.dev@gmail.com>
2019-05-10 19:23:59 +0900

    renew invalid session in web routing

--- infra/http/registerer/web_registerer.go
+++ infra/http/registerer/web_registerer.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/tomocy/ritty-for-branches/infra/http/middleware"
 	"github.com/tomocy/ritty-for-branches/infra/http/route"
 	"github.com/tomocy/ritty-for-branches/infra/http/web/handler"
 )
@@ -19,6 +20,7 @@ type WebRegisterer struct {
 }
 
 func (r *WebRegisterer) RegisterRoutes(router chi.Router) {
+	router.Use(middleware.RenewInvalidSession)
 	router.Get("/*", http.FileServer(http.Dir("resource/public")).ServeHTTP)
 	router.Get(route.Web.Route("menu.new").Path, r.handler.ShowMenuRegistrationForm)
 }
