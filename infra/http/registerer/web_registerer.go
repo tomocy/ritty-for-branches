dbdd7faee4893317f38d85d17057c0c06cf23b65
tomocy <tomocy.dev@gmail.com>
2019-05-10 18:54:12 +0900

    serve static files

--- infra/http/registerer/web_registerer.go
+++ infra/http/registerer/web_registerer.go
@@ -1,6 +1,8 @@
 package registerer
 
 import (
+	"net/http"
+
 	"github.com/go-chi/chi"
 	"github.com/tomocy/ritty-for-branches/infra/http/web/handler"
 )
@@ -15,4 +17,6 @@ type WebRegisterer struct {
 	handler *handler.Handler
 }
 
-func (r *WebRegisterer) RegisterRoutes(router chi.Router) {}
+func (r *WebRegisterer) RegisterRoutes(router chi.Router) {
+	router.Get("/*", http.FileServer(http.Dir("resource/public")).ServeHTTP)
+}
