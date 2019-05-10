ac7d7b7c51a47755ed248972ad57481f2ff44f0e
tomocy <tomocy.dev@gmail.com>
2019-05-10 15:47:16 +0900

    add RegisterRoutes to WebRegisterer

--- infra/http/registerer/web_registerer.go
+++ infra/http/registerer/web_registerer.go
@@ -1,7 +1,11 @@
 package registerer
 
+import "github.com/go-chi/chi"
+
 func NewWebRegisterer() *WebRegisterer {
 	return new(WebRegisterer)
 }
 
 type WebRegisterer struct{}
+
+func (r *WebRegisterer) RegisterRoutes(router chi.Router) {}
