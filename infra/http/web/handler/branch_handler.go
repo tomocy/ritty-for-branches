233f904f07de961c52e002b798b7630b4556b174
tomocy <tomocy.dev@gmail.com>
2019-05-13 09:42:51 +0900

    add methods to show branch profile

--- infra/http/web/handler/branch_handler.go
+++ infra/http/web/handler/branch_handler.go
@@ -35,6 +35,25 @@ type branchHandler struct {
 	storageServ service.StorageService
 }
 
+func (h *branchHandler) ShowProfile(w http.ResponseWriter, r *http.Request) {
+	id, err := session.FindAuthenticBranch(r)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	branch, err := h.repo.FindBranch(id)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	if err := h.view.Show(w, "profile.index", map[string]interface{}{
+		"Branch": branch,
+	}); err != nil {
+		logInternalServerError(w, "show profile", err)
+	}
+}
+
 func (h *branchHandler) ShowMenus(w http.ResponseWriter, r *http.Request) {
 	id, err := session.FindAuthenticBranch(r)
 	if err != nil {
